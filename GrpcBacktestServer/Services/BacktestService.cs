ad49129f4a2f0a2dabcfa94dd7c25edf62dea52b
oumaima.mamoun@grenoble-inp.org <oumaima.mamoun@grenoble-inp.org>
2024-09-04 18:08:41 +0200

    Creation library + grpc server

---
+++ GrpcBacktestServer/Services/BacktestService.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+using GrpcBacktestServer.Protos;
+
+namespace GrpcBacktestServer.Services
+{
+    public class BacktestService : BacktestRunner.BacktestRunnerBase  // Votre service doit hériter de la classe générée à partir de votre proto
+    {
+        public override Task<BacktestOutput> RunBacktest(BacktestRequest request, ServerCallContext context)
+        {
+            // Implémentez la logique de traitement du backtest ici
+            var response = new BacktestOutput
+            {
+                // Remplissez la réponse en fonction des résultats de votre backtest
+            };
+            return Task.FromResult(response);
+        }
+    }
+}
